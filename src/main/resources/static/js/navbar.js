f506abb15ad1a4bb679c4269c2c39f408aea136e
Matthew Hile <mattwhile83@gmail.com>
2025-03-10 19:10:05 -0400

    Created html files for navbar and 4 new pages

---
+++ src/main/resources/static/js/navbar.js
@@ -0,0 +1,8 @@
+// Javascript for navbar.html
+
+document.addEventListener("DOMContentLoaded", function () {
+    fetch("navbar.html")
+        .then(response => response.text())
+        .then(data => document.getElementById("navbar").innerHTML = data)
+        .catch(error => console.error("Error loading navbar:", error));
+});
